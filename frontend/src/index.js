e012f96838de1ca1618716c414a2601b46d0600c
IrishLad63 <seanpclancey@gmail.com>
2025-05-27 12:00:35 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// File: frontend/src/index.js import React from "react"; import ReactDOM from "react-dom/client"; import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root")); root.render( <React.StrictMode> <App /> </React.StrictMode> );
+
