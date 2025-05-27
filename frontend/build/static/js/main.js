b8817fb6d46cb854014640c11af6979adeb2dfcb
IrishLad63 <seanpclancey@gmail.com>
2025-05-27 14:23:35 -0500

    Delete frontend/build/static/js directory

--- frontend/build/static/js/main.js
+++
@@ -1,8 +0,0 @@
-console.log("CryptoBot dashboard booted.");
-document.getElementById("root").innerHTML = `
-  <div style='padding:40px;font-family:sans-serif'>
-    <h1>Clancey's Crazy CryptoBot</h1>
-    <p><strong>Status:</strong> Loading...</p>
-    <p><em>React app is connected to backend and will update after first API call.</em></p>
-  </div>
-`;
